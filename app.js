5dc3176c0e7d41020ec5ef54a80cccf204faad96
batch2020spring <90603986+Shahmeer-Nasir@users.noreply.github.com>
2022-06-30 23:33:44 +0500

    update

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 
-function AddPartition(){
+function AddPartition(ram_size){
+
+    size = Math.floor(Math.random() * ram_size / 2) + 2;
+    // alert('ram_size: ' + ram_size + '\nsize: ' + size);
 
     const ram = document.getElementById('ram');
 
     const node = document.createElement('div')
-    const node1 = document.createElement('div');
-    const partition_size = document.createTextNode('2 MB');
+    const node1 = document.createElement('input');
+    node1.type = 'text';
+    node1.value = size;
+    // const partition_size = document.createTextNode(size);
 
-    node1.appendChild(partition_size);
+    // node1.appendChild(partition_size);
     node.appendChild(node1);
     node.className = 'partition';
 
     ram.appendChild(node);
+
+    return ram_size - size;
 }
 
 
@@ -32,11 +39,12 @@ function UpdateType(id) {
 function UpdateMemory(id) {
 
     ram_size = document.getElementById(id).value;
-    document.getElementById('ram-size').innerHTML = ram_size + ' MB';
+    document.getElementById('ram-size').innerHTML = ram_size;
 
     let max_partition = ram_size / 2;
 
-    document.getElementById('partitions')
+    document.getElementById('partitions').max = max_partition;
+    document.getElementById('part-max').innerHTML = max_partition;
 
 }
 
@@ -44,8 +52,12 @@ function UpdatePartitions(id) {
 
     partitions = document.getElementById(id).value;
     document.getElementById('ram').innerHTML = '';
+    ram_size = document.getElementById('ram-size').innerHTML;
+    max_part_size = ram_size / 2;
+
+    // alert(ram_size - 2);
     for(i = 0; i < partitions; i++) {
-        AddPartition();
+        max_part_size = AddPartition(max_part_size);
     }
 }
 
